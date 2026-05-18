17544af9abff8faa78d36d18d0acb851d347fca5
greyw0rks <elchapidave@gmail.com>
2026-05-18 10:01:41 +0100

    Updated contract

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,9 +8,9 @@ export const USDM_ADDRESS = "0x765DE816845861e75A25fCA122bb6898B8B1282a" as cons
 // Deploy these and update:
 export const CONTRACT_ADDRESSES = {
   crashGame:        "0x8ea586bb4ce9a104aa0c2b83707ff92bbb501059" as const, // TODO: deploy
-  predictionMarket: "0xda1de2ad187cfef1b80a9896ec1915ed051c9693" as const, // TODO: deploy
-  chessWager:       "0x74cdef5b6fda93f2e669548a372f5338b76caa78" as const, // TODO: deploy
-  blackjack:        "0x72fb7312b53ef6e8ae8acb0c0b2a7d372f41c02f" as const, // ✅ deployed
+  predictionMarket: "0x31b69623c1bf3c952d31d6345e599e1ba05fcd39" as const, // TODO: deploy
+  chessWager:       "0xa63d368c5e94b9fb9c47f52392f7499a8a6f625f" as const, // TODO: deploy
+  blackjack:        "0x39e929da037b24a7f26c19216b207990a7600901" as const, // ✅ deployed
 } as const;
 
 export const USDM_DECIMALS = 18;
